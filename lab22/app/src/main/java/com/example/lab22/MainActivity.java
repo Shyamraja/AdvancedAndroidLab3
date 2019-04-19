4b50c939284ce00d9b8366cd748dea8c299e84ad
Shyamraja <t7bhsh00"students.oamk.fi>
2019-04-19 20:21:15 +0300

    added lsit view and add country to list is working

--- lab22/app/src/main/java/com/example/lab22/MainActivity.java
+++ lab22/app/src/main/java/com/example/lab22/MainActivity.java
@@ -2,52 +2,72 @@ package com.example.lab22;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
-
-
 import java.util.ArrayList;
 
-     public class MainActivity extends AppCompatActivity {
-       ArrayList<String> countries;
 
-       @Override
-       protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         //setContentView(R.layout.activity_main);
-         countries = new ArrayList<>();
-         countries.add("Afghanistan");
-         countries.add("Albania");
-         countries.add("Algeria");
-         countries.add("America Samoa");
-         countries.add("Andorra");
-         countries.add("Angola");
-         countries.add("Anguilla");
-         countries.add("Antarctica");
+public class MainActivity extends AppCompatActivity {
+  ArrayList<String> countries;
 
-
-         LinearLayout principal = new LinearLayout(this);
-         principal.setOrientation(LinearLayout.VERTICAL);
-         setContentView(principal);
-
-         LinearLayout buttons = new LinearLayout(this);
-         buttons.setOrientation(LinearLayout.HORIZONTAL);
-         Button add = new Button(this);
-         add.setText("ADD");
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    //setContentView(R.layout.activity_main);
+    countries = new ArrayList<>();
+    countries.add("Afghanistan");
+    countries.add("Albania");
+    countries.add("Algeria");
+    countries.add("America Samoa");
+    countries.add("Andorra");
+    countries.add("Angola");
+    countries.add("Anguilla");
+    countries.add("Antarctica");
 
 
-         Button edit = new Button(this);
-         edit.setText("EDIT");
+    LinearLayout mainView = new LinearLayout(this);
+    mainView.setOrientation(LinearLayout.VERTICAL);
+    setContentView(mainView);
 
-         Button remove = new Button(this);
-         remove.setText("REMOVE");
+    LinearLayout buttons = new LinearLayout(this);
+    buttons.setOrientation(LinearLayout.HORIZONTAL);
+    Button add = new Button(this);
+    add.setText("ADD");
 
 
-         buttons.addView(add);
-         buttons.addView(edit);
-         buttons.addView(remove);
+    Button edit = new Button(this);
+    edit.setText("EDIT");
 
-         principal.addView(buttons);
-       }
-     }
+    Button remove = new Button(this);
+    remove.setText("REMOVE");
+
+
+    buttons.addView(add);
+    buttons.addView(edit);
+    buttons.addView(remove);
+
+    mainView.addView(buttons);
+
+
+    final EditText edittxt = new EditText(this);
+    edittxt.setText("");
+    mainView.addView(edittxt);
+
+    ListView countries_v = new ListView(this);
+    mainView.addView(countries_v);
+
+    final ArrayAdapter<String> arraycountries = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+    countries_v.setAdapter(arraycountries);
+
+    add.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        arraycountries.add(edittxt.getText().toString());
+      }
+    });
+  }
+}
